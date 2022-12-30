b43b702c061d92254c466f776fd11bb207f0fa8c
sammyalwayscode <olorundasamuel2@gmail.com>
2022-12-30 12:54:05 +0100

    laundary

--- src/index.js
+++ src/index.js
@@ -1,31 +1,21 @@
 const express = require("express");
-const path = require("path");
 const PORT = 4032;
 const app = express();
-const router = express.Router();
 const orderRoute = require("../router/orderRoutes");
 app.use(express.json());
 require("../config/db");
 
 app.set("view engine", "ejs");
 
-router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/html/index.html"));
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Server Up 🚀🚀🚀",
+  });
 });
 
 app.get("/home", (req, res) => {
   res.render("mail");
 });
-
-// app.get("/", (req, res) => {
-//   res.status(200).json({
-//     message: "Server Up",
-//   });
-// });
-
-// router.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname + "/html/err.html"));
-// });
 
 app.use("/", router);
 app.use("/api", orderRoute);
